81c20f5b7cfc73106689e17f27f8efc37f0672e5
Johan Åkesson <johan@akesson.st>
2025-07-17 07:37:43 +0200

    new frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import { trpc } from './utils/trpc';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { trpc } from "./utils/trpc";
+import "./index.css";
+import App from "./App.tsx";
 
 const queryClient = new QueryClient();
 
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3001/trpc',
+      url: process.env.API_URL ?? "http://localhost:3001/trpc",
     }),
   ],
 });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </trpc.Provider>
-  </StrictMode>,
+  </StrictMode>
 );
